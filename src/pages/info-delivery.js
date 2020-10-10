f89d6c3c84f7fe72fa6ba1fc2f9860d0dd119d11
judahkrug <jkrug7@gatech.edu>
2020-10-10 15:01:54 -0400

    Fixed info delivery text and images

--- src/pages/info-delivery.js
+++ src/pages/info-delivery.js
@@ -9,13 +9,16 @@ const InfoDelivery = () => {
   return (
     <div className="info-background">
       <p className="info-title">Ticket</p>
+      {/* <div style={{display: "flex", justifyContent: "center"}}> */}
       <p className="info-text">
-        As you drive through, WheelRight equipment analyzes your tires with cameras and sensors. <br />
+        As you drive through, WheelRight equipment analyzes your tires with cameras and sensors.
+        <br />
         <br />
         Within seconds, WheelRight has crunched the data for every individual tire and can deliver the information
         <br />
         where and how you need it--a text message, a printed ticket, or organized in the cloud for fleet management.
       </p>
+      {/* </div> */}
       <img className="image-phone" src={Phone} alt="Phone graphic" />
       <img className="image-messages" src={Messages} alt="Message Bubbles" />
       <img className="image-ticket" src={Ticket} alt="Ticket Graphic" />
